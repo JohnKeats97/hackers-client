0c468b86bf14862b73fea232fd86052159ab1e5e
Unknown <johnbuevich@yahoo.com>
2018-04-22 16:44:59 +0300

    v9

--- Data/ServiceWorker.js
+++ Data/ServiceWorker.js
@@ -3,7 +3,7 @@ const cacheList = ["/main2.js", "/Resources/Textures/hackers.jpg",
     "https://fonts.googleapis.com/css?family=Indie+Flower",
 ];
 
-const cacheName = "Hackers Contest-v8";
+const cacheName = "Hackers Contest-v9";
 
 this.addEventListener("install", (event) =>
 {
